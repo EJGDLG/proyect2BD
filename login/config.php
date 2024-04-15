c074e95efaefdfa3e28dcfcf21328031ddda4e6a
EJGDLG <93065287+EJGDLG@users.noreply.github.com>
2024-04-15 10:26:51 -0600

    Add files via upload

---
+++ login/config.php
@@ -0,0 +1,12 @@
+<?php
+$host = 'localhost';
+$dbname = 'restaurante';
+$username = 'root';
+$password = '';
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexión exitosa";
+} catch(PDOException $e) {
+    echo "Error de conexión: " . $e->getMessage();
+}
